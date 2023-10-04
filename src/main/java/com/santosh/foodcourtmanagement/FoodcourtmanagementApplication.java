c77a476836bb6b7c425d93acbf89f2187a7870b3
smartsantosh1928 <smartsantosh1928@gmail.com>
2023-10-04 22:07:48 +0530

    initial commit

---
+++ src/main/java/com/santosh/foodcourtmanagement/FoodcourtmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.santosh.foodcourtmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcourtmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcourtmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosh/foodcourtmanagement/FoodcourtmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.foodcourtmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcourtmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
